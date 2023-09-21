cddc918f5439a097b7fbae5a9a7163cc89de81d5
Sn0wF1re <mokayaclinton34@gmail.com>
2023-09-21 11:06:00 +0300

    Add route to create a group

--- src/routes/groups.js
+++ src/routes/groups.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 // Import controller function to get group details
-const { getGroupDetails } = require('../controllers/groups');
+const { getGroupDetails, createGroup } = require('../controllers/groups');
 
 // Get group details
+router.post('/groups', createGroup);
 router.get('/groups/:groupId', getGroupDetails);
 
 module.exports = router;
