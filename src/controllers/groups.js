8b4714b00be9a8bdfc215914c326bfc3c998a082
Sn0wF1re <mokayaclinton34@gmail.com>
2023-09-21 11:05:09 +0300

    Add createGroup controller

--- src/controllers/groups.js
+++ src/controllers/groups.js
@@ -1,6 +1,16 @@
 const { Group } = require('../model/index');
 const { sendSuccessfulResponse } = require('../utils/constants/response')
 
+const createGroup = async (req, res) => {
+    try {
+        const group = await Group.create({ ...req.body });
+        return res.status(201).json({ group });
+    } catch (error) {
+        console.error(error)
+    }
+
+}
+
 const getGroupDetails = async (req, res) => {
     // Get groupId from params
     const { groupId } = req.params;
@@ -14,7 +24,7 @@ const getGroupDetails = async (req, res) => {
             res.status(404).json({ message: 'Group not found' });
         }
         // If group exists, send success response
-        return sendSuccessfulResponse(res, 200, group);
+        return res.status(200).json({ group });
         
     } catch (error) {
         console.error(`error fetching group details: ${error}`);
@@ -22,5 +32,6 @@ const getGroupDetails = async (req, res) => {
 };
 
 module.exports = {
-    getGroupDetails
+    getGroupDetails,
+    createGroup
 }
