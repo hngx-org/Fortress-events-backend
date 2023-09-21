9d549f2633a02557865a7960eb9eb4c4298e94d6
Quadri Usman <tqu14ee1065@gmail.com>
2023-09-21 18:10:16 +0100

    edited files

--- src/routes/groups.js
+++ src/routes/groups.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 // Import controller function to get group details
-const { getGroupDetails, updateGroup } = require("../controllers/groups");
+const { updateGroup } = require("../controllers/groups");
 
 // Get group details
-router.get("/groups/:groupId", getGroupDetails);
+
 router.put("/groups/:groupId", updateGroup);
 
 module.exports = router;
