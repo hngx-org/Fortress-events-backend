588229c9e7777b28b6b4bd9f6a5d89de11ac5b86
Ubong Edem <ubongedem78@gmail.com>
2023-09-21 03:50:30 +0100

    events-api

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const cors = require("cors");
 const errorHandlerMiddleware = require("./src/middlewares/error-handler");
 const notFound = require("./src/middlewares/not-found");
 
-readdirSync("./src/routes").map((path) =>
-  app.use("/", require(`./src/routes/${path}`))
-);
-
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+readdirSync("./src/routes").map((path) =>
+  app.use("/api", require(`./src/routes/${path}`))
+);
 
 app.use(errorHandlerMiddleware);
 app.use(notFound);
@@ -31,10 +31,6 @@ app.use(function (req, res, next) {
 });
 
 const PORT = process.env.PORT;
-
-app.get("/", (req, res) => {
-  res.send("i am homer");
-});
 
 app.listen(PORT || 3000, () => {
   console.log(`Event App running on http://localhost:${PORT}/`);
--- src/routes/test.js
+++ src/routes/test.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const { getAllUsers } = require("../controllers/test");
 
 router.get("/test", getAllUsers);
+router.get("/", getAllUsers);
 
 module.exports = router;
