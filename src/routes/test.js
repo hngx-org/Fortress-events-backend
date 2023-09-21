a698364578e37a142d2ebee8e870adc1963d4aaa
Ubong Edem <ubongedem78@gmail.com>
2023-09-21 01:00:28 +0100

    new commit

--- src/routes/test.js
+++ src/routes/test.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { getAllUsers } = require("../controllers/test");
 
+router.get("/test", getAllUsers);
 
 module.exports = router;
