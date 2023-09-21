78d15448fa00a8c898359ce6b0418fad389d19d8
kliuvert <galvineegbe@gmail.com>
2023-09-21 13:46:37 +0100

    updating a user using the PK

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,1 +1,20 @@
-const User = require("../models/user");
+const { User} = require('../model')
+const { NotFoundError } = require("../errors")
+
+const updateSignleUserProfile =  async(req, res) =>{
+    try{
+        const id  = req.params.id;
+        const updateProfile = await User.findByPk(id);
+        if (!updateProfile) {
+            throw new NotFoundError("User not found");
+          };
+        res.status(200).json({ updateProfile });
+    }catch(error){
+        console.log(error);
+        return res.status(404).json({ error: error.message });
+    }
+
+}
+
+
+module.exports ={updateSignleUserProfile}
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const { updateSignleUserProfile } = require('../controllers/users')
+
+router.route('/user/:id')
+.put(updateSignleUserProfile)
+
 
 
 module.exports = router;
