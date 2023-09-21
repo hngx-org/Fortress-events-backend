ad64ee46ed1560e4a68207750e8a6b21715b8102
Quadri Usman <tqu14ee1065@gmail.com>
2023-09-21 15:00:21 +0100

    updated with middleware authorization function by quadri-usman

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const errorHandlerMiddleware = require("./src/middlewares/error-handler");
 const notFound = require("./src/middlewares/not-found");
-
+const checkAuthMiddleware = require("./src/middlewares/check-auth");
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -24,7 +24,7 @@ app.use(function (req, res, next) {
 });
 
 readdirSync("./src/routes").map((path) =>
-  app.use("/api", require(`./src/routes/${path}`))
+  app.use("/api", checkAuthMiddleware.checkAuth, require(`./src/routes/${path}`))
 );
 
 app.use(errorHandlerMiddleware);
