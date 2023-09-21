9a98c9b6354f1e0daaecf817ddb8895a78873646
Taiwola <seunolanitori@gmail.com>
2023-09-21 16:42:26 +0100

    moved the business logic from the routes to controller

--- src/controllers/comments.js
+++ src/controllers/comments.js
@@ -1,48 +1,99 @@
-// const sequelize = require('sequelize');
-// const { Event, Comment} = require('../model');
-// const { NotFoundError} = require('../errors');
+const sequelize = require('sequelize');
+const { Event, Comment } = require('../model');
+const { NotFoundError } = require('../errors');
 
 
-// // Get comments from an event by Id
-// const getSingleComment = async (req, res) => {
-//     try {
-//       const eventId = req.params;
-  
-//       // Find the event by ID and include associated comments
-//       const event = await Event.findByPk(eventId, {
-//         include: Comment,
-//       });
-  
-//       if (!event) {
-//         throw new NotFoundError("Event not found");
-//       }
-//       return res.status(200).json(event.Comments);
 
-//     } catch (error) {
+// add Comments
+const addComment = async (req, res) => {
+    try {
+        const eventId = req.params.eventId;
+        // the user_id should come from a middleware
+        const { body, user_id } = req.body;
 
-//       console.error(error);
-//       return res.status(404).json({ message: error.message });
-//     }
-//   };
+        const eventExist = await Event.findOne({
+            where: { id: id },
+        });
+
+        if (!eventExist) {
+            throw new NotFoundError("Event not found");
+        }
+
+        // Create a new comment using the Comment model
+        const newComment = await Comment.create({
+            body,
+            user_id,
+            event_id: eventId,
+        });
+
+        return res.status(201).json({
+            message: "comment created successfully",
+            newComment
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+//Get comment 
+const getComment = async (req, res) => {
+    try {
+        const comments = await Comment.findAll()
+        return res.status(200).json(comments)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Internal server error' });
+}
+}
+
+// Get comments from an event by Id
+const getSingleComment = async (req, res) => {
+    try {
+        const eventId = req.params;
+
+        // Find the event by ID and include associated comments
+        const event = await Event.findByPk(eventId, {
+            include: Comment,
+        });
+
+        if (!event) {
+            throw new NotFoundError("Event not found");
+        }
+        return res.status(200).json(event.Comments);
+
+    } catch (error) {
+        console.error(error);
+        return res.status(404).json({ message: error.message });
+    }
+};
+
+const updateComment = async (req, res) => {
+    try {
+      const commentId = req.params.commentId;
+      const { body } = req.body;
   
-//         // Add a comment to an event
-//     const addComment = async (req, res) => {
-//     try {
-//       const eventId = req.params.eventId;
-//       const { body, user_id } = req.body;
+      // Find the comment by ID
+      const comment = await Comment.findByPk(commentId);
   
-//       // Create a new comment
-//       const newComment = await Comment.create({
-//         body,
-//         user_id,
-//         event_id: eventId,
-//       });
+      if (!comment) {
+        return res.status(404).json({ message: 'Comment not found' });
+      }
   
-//       return res.status(201).json(newComment);
-//     } catch (error) {
-//       console.error(error);
-//       return res.status(500).json({ message: 'Internal server error' });
-//     }
-//   };
+      // Update the comment's body
+      comment.body = body;
+      await comment.save();
   
-//   module.exports = router;
+      return res.status(200).json(comment);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  }
+
+module.exports = {
+    getSingleComment,
+    addComment,
+    getComment,
+    updateComment
+};
