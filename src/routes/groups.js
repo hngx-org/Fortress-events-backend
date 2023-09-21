fe6de0f8376e515ae2b5c2867b614cdda400b484
Sn0wF1re <mokayaclinton34@gmail.com>
2023-09-21 11:14:05 +0300

    Remove route to create a group

--- src/routes/groups.js
+++ src/routes/groups.js
@@ -2,10 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 // Import controller function to get group details
-const { getGroupDetails, createGroup } = require('../controllers/groups');
+const { getGroupDetails } = require('../controllers/groups');
 
 // Get group details
-router.post('/groups', createGroup);
 router.get('/groups/:groupId', getGroupDetails);
 
 module.exports = router;
