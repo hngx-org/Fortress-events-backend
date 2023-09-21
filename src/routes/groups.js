1cfae0f7f93161ac6112323f61638767434f73de
Quadri Usman <tqu14ee1065@gmail.com>
2023-09-21 12:20:41 +0100

    updated by quadri-usman

--- src/routes/groups.js
+++ src/routes/groups.js
@@ -1,4 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const {
+  createGroup,
+  getAllGroups,
+  getSingleGroup,
+  updateGroup,
+  deleteGroup,
+} = require("../controllers/groups");
+
+router.route("/groups").get(getAllGroups).post(createGroup);
+
+router
+  .route("/groups/:groupId")
+  .get(getSingleGroup)
+  .put(updateGroup)
+  .delete(deleteGroup);
 
 module.exports = router;
