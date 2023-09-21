16bd9440cd656651ee8e0b8509a9b22da4924451
Quadri Usman <tqu14ee1065@gmail.com>
2023-09-21 18:00:39 +0100

    delete api for groups

--- src/controllers/groups.js
+++ src/controllers/groups.js
@@ -38,7 +38,23 @@ const updateGroup = async (req, res) => {
   }
 };
 
+const deleteGroup = async (req, res) => {
+  try {
+    const { groupId } = req.params;
+    const group = await Group.destroy({ where: { id: groupId } });
+
+    if (!group) {
+      throw new NotFoundError("Group not found");
+    }
+
+    res.status(200).json({ message: "Group deleted successfully", group });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 module.exports = {
   getGroupDetails,
   updateGroup,
+  deleteGroup,
 };
--- src/routes/groups.js
+++ src/routes/groups.js
@@ -2,10 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 // Import controller function to get group details
-const { getGroupDetails, updateGroup } = require("../controllers/groups");
+const {
+  getGroupDetails,
+  updateGroup,
+  deleteGroup,
+} = require("../controllers/groups");
 
 // Get group details
 router.get("/groups/:groupId", getGroupDetails);
 router.put("/groups/:groupId", updateGroup);
+router.delete("/groups/:groupId", deleteGroup);
 
 module.exports = router;
