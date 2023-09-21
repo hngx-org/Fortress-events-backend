890223a3ff0563d6008fddcff50615e464dab9b8
Sofiyyah <sofiyyah.abidoye@decagon.dev>
2023-09-21 07:06:56 +0100

    updating multer config

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -5,12 +5,20 @@ const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, "uploads/"); // Define the destination folder for uploaded images
   },
-  filename: (req, file, callback) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
-    const ext = path.extname(file.originalname);
-    callback(null, file.fieldname + "-" + uniqueSuffix + ext);
+   filename: (req, file, cb) => {
+    // Generate a unique filename for the image
+    const uniqueFilename = `${Date.now()}-${Math.round(
+      Math.random() * 1e9
+    )}${path.extname(file.originalname)}`;
+    cb(null, uniqueFilename);
   },
 });
+//   filename: (req, file, callback) => {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
+//     const ext = path.extname(file.originalname);
+//     callback(null, file.fieldname + "-" + uniqueSuffix + ext);
+//   },
+// });
 
 const upload = multer({ storage });
 
