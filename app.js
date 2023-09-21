7bcf7beabb043ddff683ba0a1a5dd7c7ac957946
Erickb <wr.t8r1@gmai.com>
2023-09-21 01:24:15 -0700

    Set up passport for authentication

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const { readdirSync } = require("fs");
+var passport = require('./src/utils/passport');
+const session = require('express-session');
 
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
@@ -23,14 +25,43 @@ app.use(function (req, res, next) {
   next();
 });
 
+// Configure express-session middleware
+app.use(session({
+  secret: 'FZdK4nKC5cutNDlCrvs/slrMMEfpn0vkC66RoBU2fyc=',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Initialize Passport and session support
+app.use(passport.initialize());
+app.use(passport.session());
+
 readdirSync("./src/routes").map((path) =>
   app.use("/api", require(`./src/routes/${path}`))
 );
+
+app.get('/login/google', passport.authenticate('google', {
+  scope: [ 'email', 'profile' ]
+}));
+
+
+app.get('/oauth2/redirect/google',
+  passport.authenticate('google', { failureRedirect: '/login', failureMessage: true }),
+  
+  function(req, res) {
+    res.redirect('/');
+  });
+
+  app.get('/', (req, res) => {
+    res.send('Welcome to Fortress')
+  });
+    
 
 app.use(errorHandlerMiddleware);
 app.use(notFound);
 
 const PORT = process.env.PORT;
+
 
 app.listen(PORT || 3000, () => {
   console.log(`Event App running on http://localhost:${PORT}/`);
--- src/utils/passport.js
+++ src/utils/passport.js
@@ -1,22 +1,18 @@
-require('dotenv').config()
-const passport =require("passport")
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const { User } = require("../model");
+var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const passport = require('passport');
 
 passport.use(new GoogleStrategy({
-        clientID: process.env.clientID,
-        clientSecret: process.env.clientSecret,
-        callbackURL: `http://localhost:${process.env.PORT}/google/callback`,
-        passReqToCallback   : true
-},
-function(request, accessToken, refreshToken, profile, done) {
-        return done(null, profile);
-}
+    clientID:     process.env.clientID,
+    clientSecret: process.env.clientSecret,
+    callbackURL: "http://localhost:3000/auth/google/callback",
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
 ));
 
-passport.serializeUser(function(user, done) {
-done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-done(null, user);
-});
+module.exports = passport;
