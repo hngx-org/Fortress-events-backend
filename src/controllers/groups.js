922e5aed6beaff06320579e57b0e323b75049d31
Quadri Usman <tqu14ee1065@gmail.com>
2023-09-21 18:12:07 +0100

    edited

--- src/controllers/groups.js
+++ src/controllers/groups.js
@@ -1,54 +1,6 @@
-const sequelize = require("sequelize");
-const { Group, UserGroup } = require("../model");
-const { NotFoundError } = require("../errors");
-
-//create group
-const createGroup = async (req, res) => {
-  try {
-    // return res.send(req.body);
-    const group = await Group.create({ ...req.body });
-    return res
-      .status(201)
-      .json({ message: "Group successfully created", group });
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-// get all groups
-const getAllGroups = async (req, res) => {
-  try {
-    const groups = await Group.findAll({
-      attributes: {
-        exclude: ["createdAt", "updatedAt"],
-      },
-    });
-    res.status(200).json({ groups });
-  } catch (error) {
-    console.log(error);
-    return res.status(404).json({ error: error.message });
-  }
-};
-
-//get group by id
-const getSingleGroup = async (req, res) => {
-  try {
-    const { groupId } = req.params;
-    const group = await Group.findByPk(groupId);
-
-    if (!group) {
-      throw new NotFoundError("Group not found");
-    }
-
-    res.status(200).json({ group });
-  } catch (error) {
-    console.log(error);
-    return res.status(404).json({ error: error.message });
-  }
-};
+const { Group } = require("../model/index");
 
 // update group
-
 const updateGroup = async (req, res) => {
   try {
     const { groupId } = req.params;
@@ -67,26 +19,6 @@ const updateGroup = async (req, res) => {
   }
 };
 
-// delete group
-const deleteGroup = async (req, res) => {
-  try {
-    const { groupId } = req.params;
-    const group = await Group.destroy({ where: { id: groupId } });
-
-    if (!group) {
-      throw new NotFoundError("Group not found");
-    }
-
-    res.status(200).json({ message: "Group deleted successfully", group });
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 module.exports = {
-  createGroup,
-  getAllGroups,
-  getSingleGroup,
   updateGroup,
-  deleteGroup,
 };
