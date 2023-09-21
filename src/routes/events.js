6b6bb27c3742796f11f50e615efb4ad561005271
Ubongedem78 <66006259+ubongedem78@users.noreply.github.com>
2023-09-21 04:12:30 +0100

    Update events.js

--- src/routes/events.js
+++ src/routes/events.js
@@ -1,19 +1,1 @@
-const express = require("express");
-const router = express.Router();
-const {
-  createEvent,
-  getAllEvents,
-  getSingeEvent,
-  updateEvent,
-  deleteEvent,
-} = require("../controllers/events");
 
-router.route("/events").get(getAllEvents).post(createEvent);
-
-router
-  .route("/events/:eventId")
-  .get(getSingeEvent)
-  .put(updateEvent)
-  .delete(deleteEvent);
-
-module.exports = router;
