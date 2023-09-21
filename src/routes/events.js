976c067dbdbf5778c8bf2a17723c83d9ec6e4c06
Ruth Jemeli <75780097+RUTHJEMELI@users.noreply.github.com>
2023-09-21 15:35:50 +0300

    adding delete event route

--- src/routes/events.js
+++ src/routes/events.js
@@ -1,1 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const { Event } = require('../model'); // Import the Event model
 
+// DELETE route to delete an event by its ID
+router.delete('/events/:eventId', async (req, res) => {
+  try {
+    // Extract the event ID from the request parameters
+    const eventId = req.params.eventId;
+
+    // Find the event in the database by its ID
+    const event = await Event.findByPk(eventId);
+
+    // If the event doesn't exist, return a 404 Not Found response
+    if (!event) {
+      return res.status(404).json({ message: 'Event not found' });
+    }
+
+    // Delete the event from the database
+    await event.destroy();
+
+    // Respond with a success message
+    res.status(204).send();
+  } catch (error) {
+    // Handle errors, such as database errors, and return a 500 Internal Server Error response
+    console.error(error);
+    res.status(500).json({ message: 'An error occurred while deleting the event' });
+  }
+});
+
+module.exports = router;
--- src/routes/test.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllUsers } = require("../controllers/test");
-
-router.get("/test", getAllUsers);
-router.get("/", getAllUsers);
-
-module.exports = router;
