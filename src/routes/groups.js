e3d484620c41c613b9d498ff72080c1bf0c6ab23
Taiwola <seunolanitori@gmail.com>
2023-09-22 22:19:36 +0100

    group controller

--- src/routes/groups.js
+++ src/routes/groups.js
@@ -8,12 +8,27 @@ const {
   getGroupDetails,
   updateGroupDetails,
   deleteGroup,
-  deleteGroupMemberById
+  deleteGroupMemberById,
+  addUserToGroup,
+  getAllUserFromGroup,
+  getAllEventFromGroup
 } = require("../controllers/groups");
 
 // Get group details
-router.route("/groups").post(createGroup).get(getAllGroups);
-router.route("/groups/:groupId").get(getGroupDetails).put(updateGroupDetails).delete(deleteGroup);
-router.route("/groupId/members/id").delete(deleteGroupMemberById);
+router.route("/groups")
+  .post(createGroup)
+  .get(getAllGroups);
+router.route("/groups/:groupId")
+  .get(getGroupDetails)
+  .put(updateGroupDetails)
+  .delete(deleteGroup)
+router.route("/groupId/:userId")
+  .delete(deleteGroupMemberById);
+router.route('/groups/:groupId')
+  .post(addUserToGroup);
+router.route('/:groupId/events')
+  .get(getAllEventFromGroup)
+router.route('/:groupId/users')
+  .get(getAllUserFromGroup)
 
 module.exports = router;
