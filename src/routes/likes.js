669ddd3e467617d434699cc83715e295bfc4b0f9
abdullmustyy <abdulmusty03@outlook.com>
2023-09-22 14:30:24 +0100

    Feat: Implement GET/POST/DELETE like

--- src/routes/likes.js
+++ src/routes/likes.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const { createLike, removeLike, getLike } = require("../controllers/likes");
 const router = express.Router();
+
+router.post("/comments/likes", createLike);
+router.delete("/comments/likes", removeLike);
+router.get("/comments/likes/:commentId/:userId", getLike);
 
 module.exports = router;
