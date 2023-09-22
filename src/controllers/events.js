8b48f5f94fd2bf055480ff8a92970a1aeb68affb
Ruth Jemeli <75780097+RUTHJEMELI@users.noreply.github.com>
2023-09-22 13:37:41 +0300

    updated delete

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -1,1 +1,21 @@
 
+const { Event } = require("../model");
+const { NotFoundError } = require("../errors");
+
+
+const deleteEvent = async (req, res) => {
+  try {
+    const { eventId } = req.params;
+    const event = await Event.destroy({ where: { id: eventId } });
+
+    if (!event) {
+      throw new NotFoundError("Event not found");
+    }
+
+    res.status(200).json({ event });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = deleteEvent
